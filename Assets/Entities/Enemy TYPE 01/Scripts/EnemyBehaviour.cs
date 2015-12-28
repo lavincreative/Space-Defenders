c15f906a823c2d72eac7bef23a06defa3ebf4a5e
Alvaro Lavin <alvaro@labincreative.com>
2015-12-28 21:55:39 +0000

    Adición de múltiples enemigos y colisión de disparos láser
    - Adición de enemigo TIPO 01, configurados con collider 2d y su
    respectivo rigibody 2d.
    - Adición de disparo laser enemigo de forma Random con un ratio definido
    en el prefab.
    - Adición de formación para enemigo TIPO 01, con sus respectivos puntos
    de respwan y posiciones. Los enemigos TIPO 01 irán rellenando las
    posiciones vacías.
    - Adición de colisión entre disparos láser entre enemigos y nave
    jugador.
    - Tanto enemigo como jugador tienen una salud pre-establecida que se
    puede modificar a gusto en sus respectivos prefabs.
    - Laser de los enemigos desaparecen en el shedder colocado en la parte
    inferior para no crear demasiados clones, al igual que el shedder
    colocado en la parte superior para los lásers de la nave jugador.
    - Añadidos sonidos para el enemigo TIPO 01, cuando dispara, cuando es
    eliminado.
    - Añadidos sonidos en el disparo de nave jugador.

---
+++ Assets/Entities/Enemy TYPE 01/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+	public float health = 150f;
+	public int scoreValue = 150;
+
+	public GameObject projectile;
+	public float projectileSpeed = 10f;
+	public float firingRate = 0.5f;
+
+	public AudioClip fireSound;
+	public AudioClip deathSound;
+
+//	private ScoreKeeper scoreKeeper;
+
+	// Use this for initialization
+	void Start () {
+//		scoreKeeper = GameObject.Find ("Score").GetComponent<ScoreKeeper> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float prob = firingRate * Time.deltaTime;
+		if (Random.value < prob) {
+			Fire ();
+		}
+	}
+
+	void Fire() {
+		Vector3 offset = new Vector3 (0,-1,0);
+		GameObject laser = Instantiate (projectile, transform.position + offset, Quaternion.identity) as GameObject;
+		laser.GetComponent<Rigidbody2D> ().velocity = new Vector3 (0, -projectileSpeed, 0); 
+		AudioSource.PlayClipAtPoint (fireSound, transform.position);
+	}
+
+	void OnTriggerEnter2D(Collider2D collider){
+		Debug.Log (collider);
+		Projectile missile = collider.gameObject.GetComponent<Projectile>();
+		if(missile){
+			health -= missile.GetDamage();
+			missile.Hit();
+			if (health <= 0) {
+				Die();
+			}
+		}
+	}
+
+	void Die(){
+		AudioSource.PlayClipAtPoint(deathSound, transform.position);
+//		scoreKeeper.Score(scoreValue);
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Entities/Player/Scripts/PlayerController.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	private Vector3 movementVectorAxis;
+	public float speed = 8.0f;
+	public float paddingX = 1f;
+	public float paddingY = 1f;
+
+	public float smooth = 2.0f;
+	public float tiltAngle = 30.0f;
+
+	float xmin, xmax;
+	float ymin, ymax;
+
+	public GameObject projectile;
+	public float projectileSpeed;
+	public float firingRate = 0.2f;
+	public AudioClip fireSound;
+
+	public float health = 250f;
+
+	// Use this for initialization
+	void Start () {
+		RestrictPositionShip ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// Keyboard controller FIRE
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			InvokeRepeating ("Fire", 0.000001f, firingRate);
+		}
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			CancelInvoke ("Fire");
+		}
+
+		// Command "XBOX" controller FIRE
+		if (Input.GetKeyDown (KeyCode.Joystick1Button0)) {
+			InvokeRepeating ("Fire", 0.000001f, firingRate);
+		}
+		if (Input.GetKeyUp (KeyCode.Joystick1Button0)) {
+			CancelInvoke ("Fire");
+		}
+
+		// Smoothed movement of player ship getting Vertical and Horizontal values from Input Parameters
+		movementVectorAxis.y = Input.GetAxis ("Vertical") * speed * Time.deltaTime;
+		movementVectorAxis.x = Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
+		transform.position += movementVectorAxis;
+
+		// Giving smoothed Y (horizontal) rotation when player ship goes left and right
+		float tiltAroundY = Input.GetAxis("Horizontal") * tiltAngle;
+		Quaternion target = Quaternion.Euler(transform.rotation.x, -tiltAroundY, transform.rotation.z);
+		transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+
+		// Restricting the player's position to gamespace (Starting with RestrictPositionShip() method) 
+		float newX = Mathf.Clamp (transform.position.x, xmin, xmax);
+		float newY = Mathf.Clamp (transform.position.y, ymin, ymax);
+		transform.position = new Vector3 (newX, newY, transform.position.z);
+
+		//		// Arrow keys from keyboard
+		//		if (Input.GetKey (KeyCode.LeftArrow)) {			
+		////			transform.position += new Vector3 (-speed * Time.deltaTime, 0, 0);
+		//			transform.position += Vector3.left * speed * Time.deltaTime;
+		//		} else if (Input.GetKey(KeyCode.RightArrow)) {			
+		////			transform.position += new Vector3 (speed * Time.deltaTime, 0, 0);
+		//			transform.position += Vector3.right * speed * Time.deltaTime;
+		//		}
+		//
+		//		if (Input.GetKey (KeyCode.UpArrow)) {			
+		////			transform.position += new Vector3 (0, speed * Time.deltaTime, 0);
+		//			transform.position += Vector3.up * speed * Time.deltaTime;	
+		//		} else if (Input.GetKey (KeyCode.DownArrow)) {			
+		////			transform.position += new Vector3 (0, -speed * Time.deltaTime, 0);
+		//			transform.position += Vector3.down * speed * Time.deltaTime;
+		//		}
+	}
+
+	void RestrictPositionShip ()
+	{
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint (new Vector3 (1, 0, distance));
+		Vector3 downmost = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, distance));
+		Vector3 upmost = Camera.main.ViewportToWorldPoint (new Vector3 (0, 1, distance));
+		xmin = leftmost.x + paddingX;
+		xmax = rightmost.x - paddingX;
+		ymin = downmost.y;
+		ymax = upmost.y - paddingY;
+	}
+
+	void Fire(){
+		Vector3 offset = new Vector3 (0,1,0);
+		GameObject beam = Instantiate (projectile, transform.position + offset, Quaternion.identity) as GameObject;	
+		beam.GetComponent<Rigidbody2D> ().velocity = new Vector3 (0, projectileSpeed, 0);
+		AudioSource.PlayClipAtPoint(fireSound, transform.position);
+	}
+
+	void OnTriggerEnter2D(Collider2D collider){
+		Projectile missile = collider.gameObject.GetComponent<Projectile>();
+		if (missile) {
+			Debug.Log ("Player Collided with missile");
+			health -= missile.GetDamage ();
+			missile.Hit ();
+			if (health <= 0) {
+				Die ();
+			}
+		} else {
+			Debug.Log ("Not detecting projectil enemies:" + collider.name);
+		}
+	}
+
+	void Die(){
+		LevelManager man = GameObject.Find("LevelManager").GetComponent<LevelManager>();
+		man.LoadLevel("Start Menu");
+		Destroy(gameObject);
+	}
+
+
+}
